853bbc46eebaba02a221ef3d88bd52df60b2b8cd
monkey <deyen@outlook.com>
2026-01-16 12:06:13 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module TradingRobots1
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,11 @@
+// main application
+package main
+
+import "fmt"
+
+func main() {
+	// 准备db
+
+	// launch main router
+	fmt.Println("vim-go")
+}
